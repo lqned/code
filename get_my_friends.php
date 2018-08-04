2504d399033704ebc08cf5aca9dbdb0ead70b18f
Trần Duy Tiến <34581877+lqned@users.noreply.github.com>
2018-08-04 23:40:22 +0700

    Upload

---
+++ get_my_friends.php
@@ -0,0 +1,14 @@
+<?php
+    //2018-08-04 23:40:02
+    error_reporting(0);
+    set_time_limit(0);
+    $token = '';
+    $ch = curl_init();
+    curl_setopt_array($ch, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'https://graph.facebook.com/me/friends?access_token=' . $token
+    ));
+    $response = json_decode(curl_exec($ch));
+    curl_close($ch);
+
+    print_r($response->data);
