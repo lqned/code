76f610800b203a31a9d6a624adada7647a605527
Trần Duy Tiến <34581877+lqned@users.noreply.github.com>
2018-08-05 07:48:45 +0700

    Upload

---
+++ get_reactions_of_post.php
@@ -0,0 +1,19 @@
+<?php
+    //2018-08-05 07:29:25
+    set_time_limit(0);
+    error_reporting(0);
+    $post_id = '';
+    $limit = 1000;
+    $token = '';
+
+    $ch = curl_init();
+    curl_setopt_array($ch, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => "https://graph.facebook.com/$post_id?fields=reactions.limit($limit)&access_token=$token"
+    ));
+
+    $response = json_decode(curl_exec($ch));
+
+    curl_close($ch);
+
+    print_r($response->reactions->data);
